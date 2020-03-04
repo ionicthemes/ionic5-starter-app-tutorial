9c14a329dca074c8ac3c489ec533e2234610190f
Agustin <agustinhaller@gmail.com>
2020-03-04 13:26:20 -0300

    add fav animation

--- src/app/contact-details/contact-details.page.ts
+++ src/app/contact-details/contact-details.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Contact } from '../models/contact';
+import { IonIcon, Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.page.html',
   styleUrls: ['./contact-details.page.scss'],
 })
-export class ContactDetailsPage implements OnInit {
+export class ContactDetailsPage implements OnInit, AfterViewInit {
+  @ViewChild('favIcon', { static: false, read: ElementRef }) favIcon: ElementRef;
+
   public contact: Contact;
+  public favState = false;
+  public favOnAnimation: Animation;
+  public favOffAnimation: Animation;
 
   constructor(
     private dataService: DataService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
-  ) { }
+    private router: Router,
+    private animationCtrl: AnimationController
+  ) {
+  }
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
@@ -27,4 +35,53 @@ export class ContactDetailsPage implements OnInit {
     }
   }
 
+  ngAfterViewInit() {
+    console.log('favIcon', this.favIcon);
+
+    this.favOnAnimation = this.animationCtrl.create()
+    .addElement(this.favIcon.nativeElement)
+    .duration(300)
+    // .beforeStyles({
+    //   fill: 'grey'
+    // })
+    .afterStyles({
+      fill: 'red'
+    })
+    .afterAddClass('fav')
+    .afterClearStyles(['fill'])
+    .keyframes([
+      { offset: 0, transform: 'scale(1)', opacity: '0.4' },
+      { offset: 0.5, transform: 'scale(1.5)', opacity: '0.8' },
+      { offset: 1, transform: 'scale(1)', opacity: '1' }
+    ]);
+
+    this.favOffAnimation = this.animationCtrl.create()
+    .addElement(this.favIcon.nativeElement)
+    .duration(500)
+    // .beforeStyles({
+    //   fill: 'red'
+    // })
+    .afterStyles({
+      fill: 'grey'
+    })
+    .afterRemoveClass('fav')
+    .afterClearStyles(['fill'])
+    .keyframes([
+      { offset: 0, transform: 'scale(0.8)', opacity: '0.4' },
+      { offset: 0.5, transform: 'scale(0.5)', opacity: '0.8' },
+      { offset: 1, transform: 'scale(1)', opacity: '1' }
+    ]);
+  }
+
+  fav(): void {
+    if (this.favState) {
+      this.favOnAnimation.stop();
+      this.favOffAnimation.play();
+    } else {
+      this.favOffAnimation.stop();
+      this.favOnAnimation.play();
+    }
+
+    this.favState = !this.favState;
+  }
 }
